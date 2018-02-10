110e4bb5ded6c32e8705b7e1be35a9de046d0893
Margarita Krutikova <margarita.krutikova@starrepublic.com>
2018-02-10 11:37:04 +0100

    Add cors, make minor changes to api for paginated words

--- schema/queries/getPaginatedWords.js
+++ schema/queries/getPaginatedWords.js
@@ -1,40 +1,40 @@
 const {
-  GraphQLObjectType,
-  GraphQLInt,
-  GraphQLList,
-  GraphQLNonNull,
-  getNullableType
+    GraphQLObjectType,
+    GraphQLInt,
+    GraphQLList,
+    GraphQLNonNull,
+    getNullableType
 } = require('graphql');
 
 const mongoDb = require('../../database/mongodb');
 const WordType = require('../types/word');
 
 const PaginatedWordsType = new GraphQLObjectType({
-  name: 'PaginatedWordsType',
+    name: 'PaginatedWordsType',
 
-  fields: () => ({
-    total: { 
-      type: GraphQLInt,
-      resolve: pagination => (pagination.total)
-    },
-    words: {
-      type: new GraphQLList(WordType),
-      resolve: pagination => getNullableType(pagination.words)
-    }
-  })
+    fields: () => ({
+        total: {
+            type: GraphQLInt,
+            resolve: pagination => (pagination.total)
+        },
+        items: {
+            type: new GraphQLList(WordType),
+            resolve: pagination => getNullableType(pagination.words)
+        }
+    })
 });
 
-module.exports ={
-  name: 'GetPaginatedWordsQuery',
+module.exports = {
+    name: 'GetPaginatedWordsQuery',
 
-  type: PaginatedWordsType,
-  description: 'Paginated words defined by the page and number of words per page',
-  args: {
-    page: { type: new GraphQLNonNull(GraphQLInt) },
-    itemsPerPage: { type: new GraphQLNonNull(GraphQLInt) }
-  },
+    type: PaginatedWordsType,
+    description: 'Paginated words defined by the page and number of words per page',
+    args: {
+        page: { type: new GraphQLNonNull(GraphQLInt) },
+        itemsPerPage: { type: new GraphQLNonNull(GraphQLInt) }
+    },
 
-  resolve: (obj, args, { mPool }) => {
-    return mongoDb(mPool).getWords(args);
-  }
+    resolve: (obj, args, { mPool }) => {
+        return mongoDb(mPool).getWords(args);
+    }
 };
